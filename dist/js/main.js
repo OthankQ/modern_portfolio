2057969869c1ee1de37d3e92c7c63b1ea2114c74
OthankQ <kim1885@purdue.edu>
2019-02-23 06:52:56 +0900

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
+const menuPortrait = document.querySelector(".portrait");
 const navItems = document.querySelectorAll(".nav-item");
 
 //Set Initial State Of Menu
@@ -16,6 +17,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
+    menuPortrait.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     // Set Meny State
@@ -25,6 +27,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
+    menuPortrait.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     // Set Meny State
